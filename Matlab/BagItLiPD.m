756e33b45d03e81df24e78f32cacf24c169f7ccf
nickmckay <nick@mckays.us>
2015-11-17 12:30:58 -0700

    Matlab utility updates

---
+++ Matlab/BagItLiPD.m
@@ -0,0 +1,16 @@
+folders=dir;
+folders={folders(find(cell2mat({folders.isdir}))).name}';
+folders=folders(find(~strcmp('.',cellfun(@unique,folders,'UniformOutput',0))));
+
+for i=1:length(folders)
+    cd([folders{i}])
+    if length(dir('data'))==0 %see if it's already been bagged
+        cd ..
+        system(['/Library/Frameworks/Python.framework/Versions/3.4/bin/bagit.py "' folders{i} '"']);
+    else
+    cd ..
+    end
+    zip([folders{i}], folders{i});
+    movefile([folders{i} '.zip'],[folders{i} '.lpd']);
+    delete([folders{i} '.zip']);
+end
