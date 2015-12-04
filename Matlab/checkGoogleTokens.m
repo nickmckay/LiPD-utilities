077d701a10b0c9dd2cd1706b20064b8e6da95886
nickmckay <nick@mckays.us>
2015-12-04 08:40:16 -0700

    make checkTokens better

--- Matlab/checkGoogleTokens.m
+++ Matlab/checkGoogleTokens.m
@@ -2,7 +2,7 @@
 load('google_tokens.mat');
 
 %see how long since last refresh; update if more than an hour
-if ((now-lastUpdated)*24*60)>60
+if ((now-lastUpdated)*24*60)>30
     
     % to refresh the Docs access token (usually expires after 1 hr) you'd call
     aTokenDocs=refreshAccessToken(client_id,client_secret,rTokenDocs);
